d3a649f191aa8c083a5e3c448afc1d07fc325459
ulises-mancilla <mancilla.ulises@gmail.com>
2018-04-11 16:22:18 -0500

    First commit

---
+++ src/main/java/com/virtualpairprogrammers/messagetesting/MessagetestingApplication.java
@@ -0,0 +1,12 @@
+package com.virtualpairprogrammers.messagetesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagetestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagetestingApplication.class, args);
+	}
+}
